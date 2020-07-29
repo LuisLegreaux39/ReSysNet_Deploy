5364dbb3ca8753206218531107dd8a48f15724d0
LuisLegreaux39 <lleg231196r@gmail.com>
2020-07-29 17:45:02 -0400

    Deploy version 1

---
+++ precache-manifest.06f78e416fa05701315a02a8e1d8dad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b861ae19b8ffadc6556e52212f7318",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8301065405b330044832",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "b21e22132b08f51633a0",
+    "url": "/static/css/main.618f13dd.chunk.css"
+  },
+  {
+    "revision": "8301065405b330044832",
+    "url": "/static/js/2.444f139c.chunk.js"
+  },
+  {
+    "revision": "4680f30b16a053b882d54f36b7f508ad",
+    "url": "/static/js/2.444f139c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21e22132b08f51633a0",
+    "url": "/static/js/main.f49e6a54.chunk.js"
+  },
+  {
+    "revision": "2066410a7ecd89061cf7",
+    "url": "/static/js/runtime-main.55dbfe3d.js"
+  }
+]);
